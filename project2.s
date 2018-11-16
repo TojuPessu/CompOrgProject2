39008dda6333c5dbd1d799b40ec1dd757b834150
TojuPessu <44123507+TojuPessu@users.noreply.github.com>
2018-11-16 14:49:53 -0500

    Update project2.s
    this gets the user input

--- project2.s
+++ project2.s
@@ -5,6 +5,16 @@ invalid_empty: .asciiz "Input is empty."
 input_string: .space 40000
 .text
 
+main:
+#getting user_input
+li $v0, 8  
+la $a0, input_string
+li $a1, 40000
+syscall
+add $t1, $0, 0 
+add $t3, $0, 0 
+add $t7, $0, 2
+
 
 
 
